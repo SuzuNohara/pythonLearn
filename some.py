8811623082dfdfbcb14801a7d8ebc4ca206d472d
Suzu Nohara <suzu.nkdca@gmail.com>
2018-09-21 06:03:29 -0500

    functions

---
+++ some.py
@@ -0,0 +1,16 @@
+def function1():
+    lista = ["some","thing","more"];
+    some = "something";
+
+    lista.append("other");
+
+    if some == "something" :
+        print("son iguales");
+        print("o eso creo");
+        lista.insert(1,"some");
+        print(lista);
+        print(len(lista));
+
+function1();
+print("\n\n");
+function1();
